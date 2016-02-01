76740ad10abbd8d5d977c91d30e5b9b5ff0ee7b2
Bilkit <githinjibcw@hotmail.com>
2016-02-01 10:21:28 -0800

    changed from 8 to 16 bit counter

--- mp1/counter_8.v
+++ mp1/counter_8.v
@@ -8,19 +8,19 @@ up_down  ,  // up_down control for counter
 clk      ,  // clock input
 reset       // reset input
 );
-//------------Input Ports-------------- 
+//------------Input Ports--------------
 input up_down, clk, reset;
 //------------Internal Variables--------
-output reg [7:0] out;
+output reg [15:0] out;
 //-------------Code Starts Here-------
 always @(posedge clk)
 if (reset) begin // active high reset
-  out <= 8'b0 ;
+    out <= 8'b0 ;
 end else if (up_down) begin
-  out <= out + 1'b1;
+    out <= out + 1'b1;
 end else begin
-  out <= out - 1'b1;
+    out <= out - 1'b1;
 end
 
-endmodule 
+endmodule
 `endif
