5d9684539f29b642ba2e43c44687a58fe2ee1237
Bilkit <githinjibcw@hotmail.com>
2016-02-09 10:38:27 -0800

    simple dff with reset

---
+++ mp3/d_flipflop.v
@@ -0,0 +1,25 @@
+`timescale 1ns/10ps
+`ifndef _D_FLIPFLOP
+`define _D_FLIPFLOP
+//-----------------------------------------------------
+module d_flipflop   (
+ q     // Output signal 
+,d     // Data input
+,clk   // clock input
+,reset // reset input
+);
+//------------Input Ports-------------- 
+input logic d, clk, reset;
+//------------Internal Variables--------
+output logic q;
+//logic q; // holds state
+//-------------Code Starts Here-------
+always @(posedge clk)
+if (reset) begin // active high reset
+  q <= 0;
+end else begin
+  q <= d;
+end
+
+endmodule 
+`endif
