fdf94585d89a190e78e8919ed7a0fd21ca26c1c6
Bilkit <githinjibcw@hotmail.com>
2016-03-02 21:35:38 -0800

    extracting delay values. TODO: calc averages, formatted print, and cleanup

--- mp6/extractLogicDelay.py
+++ mp6/extractLogicDelay.py
@@ -1,33 +1,61 @@
 #!/usr/bin/python3
 
 import os.path as osp
+import glob
 import re
 
-PATTERN = ""
+NUMBER_RE = '\d\.\d*e-\d*'
+DELAY_RE = 'Total \w* delay: '
 
-# returns NULL if failed to load file
+# load file and extract and return lines
+# returns NULL if failed to load file.
+# ARGS: filepath
+# RETURN: list of files
 def get_lines(fpath):
-   print("loading: "+fpath)
    # check that file exists
    if(not osp.isfile(fpath)):
       print("(!) ERROR: "+fpath+" does not exist.")
       return None
 
    # open and read lines
-   f = open('file_name.ext', 'r')
+   f = open(fpath, 'r')
    return f.readlines()
 
-# search for this pattern
-# “Total logic delay: 8.98824e-10 (s) Total net delay: 6.44429e-10 (s) ”
+# search lines for match and return the matching string
+# return NULL if no match exists in list.
+# ARGS: list of strings to search
+# RETURN: list of matches found in list
+def find_match(lines):
+   results = [0,0]
+   for ll in lines:
+      # get line mentioning delays
+      delay_lines = re.findall(r''+DELAY_RE+NUMBER_RE,ll)
+      if(len(delay_lines) != 0):
+         # print(delay_lines)
+         # extract delay values from first two lines
+         results[0] = re.findall(r''+NUMBER_RE,delay_lines[0])[0]
+         results[1] = re.findall(r''+NUMBER_RE,delay_lines[1])[0]
+         # make sure to return only when delay line has two numbers
+         # print(results)
+         return results
+
+   return None
+
 
 def main():
-   x = re.compile(PATTERN)
-
    #for all files in current directory
+   for filename in glob.glob('./logfiles/*.txt'):
       # load and get lines
-      lines = get_lines(fname)
+      lines = get_lines(filename)
+      # print( filename+":"+str(len(lines)) )
 
+      # get numbers
+      matches = find_match(lines)
 
+      # report file and numbers
+      print(filename)
 
    return
+
+main()
 
